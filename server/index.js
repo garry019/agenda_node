8fc2c2786f2c42fdcb3c5f1d4615ff98f7d2c540
Gabriel Calderon <garry019@gmail.com>
2019-02-12 14:21:01 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 var http = require('http')
 var express = require('express')
 var routing = require('./requestRouting.js')
+var path = require('path');
 
 
 var PORT = 3000
 var app = express()
 app.use(routing)
-
+var publicPath = path.resolve(__dirname, 'client');
+app.use(express.static(publicPath));
 var Server = http.createServer(app)
 Server.listen(PORT, function(){
   // Poner un mensaje en la consola
